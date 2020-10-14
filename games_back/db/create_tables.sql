9e787063e91875dd4c7a29afd871c46559e8433f
ZettZet <dmesser@yandex.ru>
2020-10-14 17:14:16 +0500

    first commit

---
+++ games_back/db/create_tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS games(
+    game_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(10) NOT NULL,
+    dicription TEXT,
+    price INT NOT NULL CHECK(price > 0),
+);
+CREATE TABLE IF NOT EXISTS customer(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+);
+CREATE TABLE IF NOT EXISTS cart(
+    game_id INT NOT NULL,
+    cart_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    status_cart BOOLEAN NOT NULL DEFAULT(FALSE),
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
