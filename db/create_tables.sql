8460ddcfde5f660a0222ce88b63bac772b65dec5
Winkel <dmesser@yandex.ru>
2020-12-15 02:51:13 +0500

    initial

---
+++ db/create_tables.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS game_db;
+USE game_db;
+CREATE TABLE IF NOT EXISTS category(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+CREATE TABLE IF NOT EXISTS games(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(10) NOT NULL,
+    description TEXT,
+    price INT NOT NULL CHECK(price > 0),
+    uri_image VARCHAR(256) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS game_category(
+    game_id BIGINT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS customers(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(20) NOT NULL,
+    pass VARCHAR(20) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS carts(
+    game_price INT NOT NULL CHECK(game_price >= 0),
+    status ENUM('taken', 'payed'),
+    game_id BIGINT NOT NULL,
+    customer_id BIGINT,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE
+    SET NULL ON UPDATE CASCADE
+);
