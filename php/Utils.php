e6522aba0a661f63f5b376e777aadf24112bdf16
Winkel <dmesser@yandex.ru>
2020-12-28 23:10:01 +0500

    все исправлено(попытка 2)

--- php/Utils.php
+++ php/Utils.php
@@ -21,7 +21,7 @@ class Utils
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
-        return new PDO('mysql:host=db;dbname=game_db', 'devuser', 'devpass', $options);
+        return new PDO('mysql:host=db;dbname=game_db', 'root', 'my_secret_pw', $options);
     }
 
     #[Pure] public static function renderTable(array $array): string
